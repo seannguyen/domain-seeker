b4de664a0edd628f7a703f195beed4b565523cfb
Sean Nguyen <sean.nguyentrong@gmail.com>
2017-05-06 08:23:43 +0800

    Save check result to a file

--- domain-seeker.py
+++ domain-seeker.py
@@ -1,13 +1,19 @@
 from namecheap import Api
 from urllib2 import urlopen
+import itertools
+from string import ascii_lowercase
+import json
 
 import configs
 
 
 def seek(tld):
-    availabilities = check_availability(['us.xyz', 'honestbee.com'])
-    for domain, availability in availabilities.iteritems():
-        print("%s: %s" % (domain, availability))
+    domains = get_all_domain_name(tld)
+    for i in xrange(0, len(domains) - 1, 100):
+        print('Checking domain in range %s-%s/%s' % (i, i+100, len(domains)))
+        partly_result = check_availability(domains[i:min(i+100, len(domains))])
+        with open('result.json', 'a') as result_file:
+            json.dump(partly_result, result_file, indent=4)
 
 
 def check_availability(domains):
@@ -20,6 +26,12 @@ def check_availability(domains):
 
 def get_current_machine_public_ip():
     return urlopen('http://ip.42.pl/raw').read()
+
+def get_all_domain_name(tld):
+    alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    domains = [''.join(i) + '.' + tld for i in itertools.product(alphabets, repeat = 3)]
+    return domains
 
 
 if __name__ == '__main__':
