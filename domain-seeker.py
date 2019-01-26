e9331540ef78ae07033343b59cc4fbc92f837117
Sean Nguyen <sean.nguyentrong@gmail.com>
2019-01-26 12:42:35 +0800

    Update project to use sqlite

--- domain-seeker.py
+++ domain-seeker.py
@@ -1,10 +1,10 @@
 from namecheap import Api
 from urllib2 import urlopen
 import itertools
-import json
 import argparse
 import time
 import configs
+import sqlite3
 
 
 def main():
@@ -16,25 +16,42 @@ def main():
 
 
 def seek(tld, length):
+    db_conn = prepare_db_connection()
     domains = get_all_domain_name(tld, length)
     domains_per_query = 50
     for i in xrange(0, len(domains) - 1, domains_per_query):
         domains_query_end_range = min(i+domains_per_query, len(domains))
         print('Checking domain in range %s-%s/%s' % (i, domains_query_end_range, len(domains)))
         partly_result = check_availability(domains[i:domains_query_end_range])
-        with open('result.json', 'a') as result_file:
-            json.dump(partly_result, result_file, indent=4)
+        db_conn.executemany("INSERT INTO domains (domain, available) values (?, ?);", partly_result)
+    db_conn.commit()
+    db_conn.close()
+
+
+def prepare_db_connection():
+    db_conn = sqlite3.connect('./domain-seeker.sqlite')
+    db_conn.execute('''
+            CREATE TABLE IF NOT EXISTS domains(
+                domain VARCHAR(255) PRIMARY KEY,
+                available BOOLEAN NOT NULL ,
+                created_at DATETIME NOT NULL DEFAULT (datetime('now')),
+                updated_at DATETIME NOT NULL DEFAULT (datetime('now'))
+            );
+        ''')
+    return db_conn
 
 
 def check_availability(domains, retry_count=0):
     if retry_count > 5:
-        return {}
+        return []
     print('Try %s' % (retry_count + 1))
     client_ip = get_current_machine_public_ip()
-    api = Api(configs.username, configs.api_key, configs.username, client_ip,
-              sandbox=False, debug=False)
+    api = Api(configs.username, configs.api_key, configs.username, client_ip, sandbox=False, debug=False)
     try:
-        result = api.domains_check(domains)
+        domain_statuses = api.domains_check(domains)
+        result = []
+        for domain, status in domain_statuses.iteritems():
+            result.append([domain, status])
     except Exception as error:
         print(error)
         time.sleep(10)
@@ -47,8 +64,8 @@ def get_current_machine_public_ip():
 
 
 def get_all_domain_name(tld, length):
-    alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                 '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     domains = [''.join(i) + '.' + tld for i in itertools.product(alphabets, repeat=length)]
     return domains
 
